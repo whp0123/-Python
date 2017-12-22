9a7c94ab9e7e90e6aed2bff2a5463204b5c0cf7d
whp0123 <odin0123@qq.com>
2017-12-22 22:55:47 +0800

    练习1、2、3

---
+++ Exercise_3/ex3.py
@@ -0,0 +1,28 @@
+# + plus; - minus; / slash; * asterisk; % percent; 
+# < less-than; > greater-than; <= less-than-equal; >= greater-than-equal
+# explain my action: count my chickkens
+print "I will now count my chickens:"
+# Hens's count is : 25 plus (30 slash 6)
+print "Hens", 25+30/6
+# Roosters's num is : 100 minus (25 asterisk 3, then percent 4)
+print "Roosters", 100-25*3%4
+# count eggs's num:
+print "Now I will count the eggs:"
+# 3 plus 2 plus 1 minus 5 plus (4 percent 2) minus (1 slash 4) plus 6
+print 3+2+1-5+4%2-1/4+6
+# is it ture: 3 plus 2 is less than (5 minus 7)
+print "Is it true that 3+2<5-7?"
+# (3 plus 2) is less than (5 minus 7)
+print 3+2<5-7
+# 3 plus 2 = ?
+print "What is 3+2?",3+2
+# 5 minus 7 = ?
+print "What is 5-7?",5-7
+print "Oh, that's why it's false."
+print "How about some more."
+# is it ture: 5 greater than -2
+print "Is it greater?", 5>-2
+# is it ture: 5 greater-than-equal -2
+print "Is it greater or equal?",5>=-2
+# is it ture: 5 less-than-equal -2
+print "Is it less or equal?",5<=-2
